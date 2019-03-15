88c1550623336a9f4da622249f516bb0c54038cd
Jamshaid Azeem <jamshaidazeem@gmail.com>
2019-03-15 16:49:42 +0500

    start script added to package.json and heroku port setup with PORT env variable

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 // view engine setup, npmjs.com/package/hbs
@@ -65,6 +67,6 @@ app.get('/about', (req, res) => {
    });
 });
 
-app.listen(3000, () => {
-    console.log("server.js listening on port 3000...");
+app.listen(port, () => {
+    console.log(`server.js listening on port ${port}...`);
 });
